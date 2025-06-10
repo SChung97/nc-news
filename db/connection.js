7936ef35f4faba21e93df0332232aa1533ef540f
SChung97 <steph9chung@hotmail.com>
2025-06-10 12:37:39 +0100

    updated package json

--- db/connection.js
+++ db/connection.js
@@ -4,7 +4,6 @@ const ENV = process.env.NODE_ENV || "development";
 
 require("dotenv").config({ path: `${__dirname}/../.env.${ENV}` });
 console.log({ path: `${__dirname}/../.env.${ENV}` });
-const db = new Pool();
 
 if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
   throw new Error(" PGDATABASE or DATABASE_URL not set");
@@ -14,7 +13,7 @@ if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
 
 const config = {};
 if (ENV === "production") {
-  config.connecrionString = process.env.DATABASE_URL;
+  config.connectionString = process.env.DATABASE_URL;
   config.max = 2;
 }
 module.exports = new Pool(config);
