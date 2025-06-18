0ecb68f71d9303d406d29f542b24382bfc7890b6
SChung97 <steph9chung@hotmail.com>
2025-06-18 16:06:39 +0100

    addition of api documentation

--- controllers/api.controller.js
+++ controllers/api.controller.js
@@ -1,8 +1,7 @@
 const endpointsJson = require("../endpoints.json");
 
-const getEndPointsJSON = (request, response, ) => {
+const getEndPointsJSON = (request, response) => {
   response.status(200).send({ endpoints: endpointsJson });
- 
 };
 
 module.exports = { getEndPointsJSON };
