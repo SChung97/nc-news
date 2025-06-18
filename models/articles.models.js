bd8fccfed42a9ef8c8d7412d5d2df1ef1882f992
SChung97 <steph9chung@hotmail.com>
2025-06-18 21:52:38 +0100

    addition of comment count on a single article array

--- models/articles.models.js
+++ models/articles.models.js
@@ -46,7 +46,7 @@ const fetchArticleById = (id) => {
   console.log("hello from articles model");
   return db
     .query(
-      `SELECT author, title, article_id, body, topic, created_at, votes, article_img_url FROM articles WHERE article_id = $1`,
+      `SELECT articles.author, articles.title, articles.article_id, articles.body, articles.topic, articles.created_at, articles.votes, articles.article_img_url, CAST(COUNT(comments.comment_id) AS INT) AS comment_count FROM articles LEFT JOIN comments ON articles.article_id = comments.article_id WHERE articles.article_id = $1 GROUP BY articles.article_id, articles.author, articles.title, articles.body, articles.topic, articles.created_at, articles.votes, articles.article_img_url`,
       [id]
     )
     .then(({ rows }) => {
