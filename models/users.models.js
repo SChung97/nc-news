4a45ff7a5db69f552ffcbdd451f105d477e24bd4
SChung97 <steph9chung@hotmail.com>
2025-08-06 09:24:45 +0100

    removeal of console logs

--- models/users.models.js
+++ models/users.models.js
@@ -2,7 +2,6 @@ const { response } = require("../app");
 const db = require("../db/connection");
 
 const fetchUsers = () => {
-  console.log("hello from users model");
   return db.query(`SELECT * FROM users`).then(({ rows }) => {
     return rows;
   });
